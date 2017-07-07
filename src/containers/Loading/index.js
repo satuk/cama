4292faf7b97f74c38a00669c214d96a7aef89020
Satuk Kolan <34@satuk.ch>
2017-07-07 16:45:11 +0200

    implemented the linearProgress

--- src/containers/Loading/index.js
+++ src/containers/Loading/index.js
@@ -1,3 +1,32 @@
 /**
  * Created by satuk on 07.07.17.
  */
+import React from "react";
+import PropTypes from "prop-types";
+import {createStyleSheet, withStyles} from "material-ui/styles";
+import {LinearProgress} from "material-ui/Progress";
+
+const styleSheet = createStyleSheet('Loading', theme => ({
+  root: {
+    width: '100%',
+    marginTop: 64,
+  },
+}));
+
+
+const Loading = (props) => {
+  const { classes } = props;
+
+  return (
+    <div className={classes.root}>
+      <LinearProgress />
+    </div>
+
+  );
+};
+
+Loading.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styleSheet)(Loading);
