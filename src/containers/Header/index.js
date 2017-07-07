304f46c8bad49e326e29b74e4190603f22a0f9ae
Satuk Kolan <34@satuk.ch>
2017-07-07 09:16:25 +0200

    added a simple to header with material-ui components

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -2,6 +2,15 @@
  * Created by satuk on 04.07.17.
  */
 import React, {Component} from "react";
+import {Link} from "react-router-dom";
+import {Toolbar, ToolbarGroup, ToolbarTitle} from "material-ui/Toolbar";
+import {connect} from "react-redux";
+import TextField from "material-ui/TextField";
+import RaisedButton from "material-ui/RaisedButton";
+import FlatButton from "material-ui/FlatButton";
+import {blue500, blue50} from "material-ui/styles/colors";
+import {search} from 'material-ui/svg-icons/action/search'
+import qs from "query-string";
 
 const appName = "cama";
 const styles = {
@@ -20,15 +29,56 @@ const styles = {
     marginBottom: 50,
   }
 };
+
+const Login = () => {
+  return (
+    <div>
+      <Link to="/sign-in"><FlatButton style={styles.buttonTextColor} label="Sign In"/></Link>
+      <Link to="/sign-up"><FlatButton style={styles.buttonTextColor} label="Sign Up"/></Link>
+    </div>
+  );
+}
+
+const Logged = () => {
+  return (
+    <div>
+      <Link to="/edit-profile"><FlatButton style={styles.buttonTextColor} label="Edit Profile"/></Link>
+      <Link to="/sign-out"><FlatButton style={styles.buttonTextColor} label="Sign Out"/></Link>
+    </div>
+  );
 };
 
 class Header extends Component {
   render() {
     return (
-      <div>
-        <div>
-          <h1>Header</h1>
-        </div>
+      <div style={styles.container}>
+        <Toolbar style={styles.toolbar}>
+          <ToolbarGroup firstChild={true}>
+            <Link to="/" style={styles.app}><ToolbarTitle text={appName}/></Link>
+            <Link to="/register/staff-member"><FlatButton style={{ color: 'white' }}
+                                                          label="Become a Staff-Member"/></Link>
+            <Link to="/register/event-manager"><FlatButton style={{ color: 'white' }}
+                                                           label="Become a Event-Manager"/></Link>
+          </ToolbarGroup>
+
+          <ToolbarGroup>
+            <TextField
+              hintText="Find Events to apply"
+              onChange={this.handleSearchBoxOnChange}
+              value={this.state.searchEvent}
+              type="input"
+              underlineStyle={{ borderColor: 'white' }}
+              underlineFocusStyle={{ borderColor: 'white' }}
+              hintStyle={{ color: 'white' }}
+              floatingLabelStyle={{ color: 'white' }}
+            />
+            <RaisedButton label="Search" style={{ color: 'white' }} onClick={this.handleButtonOnClick}/>
+          </ToolbarGroup>
+
+          <ToolbarGroup lastChild={true}>
+            {this.state.logged ? <Logged /> : <Login />}
+          </ToolbarGroup>
+        </Toolbar>
         <div style={styles.content}>
           {
             this.props.children
