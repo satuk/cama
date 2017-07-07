3626e22555bbe84dc111d4f1652fccc18d429700
Satuk Kolan <34@satuk.ch>
2017-07-07 16:43:54 +0200

    created index.css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 import {Provider} from "react-redux";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
