c6b2f17c8d46c219291685056da7b5a883862a23
Satuk Kolan <34@satuk.ch>
2017-07-07 16:45:21 +0200

    changed the material-ui theme to dark

--- src/index.js
+++ src/index.js
@@ -3,25 +3,33 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 import {Provider} from "react-redux";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import {MuiThemeProvider, createMuiTheme} from "material-ui/styles";
+import createPalette from 'material-ui/styles/palette';
 import injectTapEventPlugin from "react-tap-event-plugin";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 import Header from "./containers/Header";
 import Home from "./routes/Home";
+import EventList from "./containers/EventList";
 
 import store from "./store";
 
+const theme = createMuiTheme({
+  palette: createPalette({
+    type: 'dark',
+  }),
+});
 
 injectTapEventPlugin();
 
 ReactDOM.render(
   <Provider store={store}>
-    <MuiThemeProvider>
+    <MuiThemeProvider theme={theme}>
       <Router>
         <Switch>
           <Header>
             <Route exact path={"/"} component={ Home }/>
+            <Route exact path={"/events"} component={ EventList }/>
           </Header>
         </Switch>
       </Router>
