c19e0ec7545741cbf1d7cc7400b620bd5b1347d2
Satuk Kolan <34@satuk.ch>
2017-07-10 21:39:49 +0200

    refactored container component

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -5,4 +5,4 @@ import React from 'react';
 import './index.css';
 
 export default ({ children }) =>
-  <div className="Home-container">{ children }</div>;
+  <div className="Home-Container">{ children }</div>;
