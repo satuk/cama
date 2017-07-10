fae8c419fb5d3b9e924fbd45cd2520e445a14cd2
Satuk Kolan <34@satuk.ch>
2017-07-10 21:42:40 +0200

    fetching all events and listing

--- src/routes/Events/index.js
+++ src/routes/Events/index.js
@@ -1,3 +1,40 @@
 /**
  * Created by satuk on 10.07.17.
  */
+import React, {Component} from "react";
+import EventList from "../../components/EventList";
+import {fetchEvents} from "../../store/actions/events";
+import {connect} from "react-redux";
+import Loading from "../../components/Loading";
+
+class Events extends Component {
+  componentDidMount() {
+    this.props.fetchEvents();
+  }
+
+  render() {
+    const { events } = this.props;
+
+    if ( !this.props.events.length ) {
+      return <Loading />;
+    }
+
+    return (
+      <EventList events={events} />
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  const events = Object.values(state.events);
+
+  return {
+    events,
+  }
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchEvents: () => dispatch(fetchEvents()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Events);
