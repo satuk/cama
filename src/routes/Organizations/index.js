ab38b1ffd8a0ca53f2cfbacb010ab7fbb124ecc0
Satuk Kolan <34@satuk.ch>
2017-07-10 21:42:57 +0200

    showing all events by a company

--- src/routes/Organizations/index.js
+++ src/routes/Organizations/index.js
@@ -1,3 +1,15 @@
 /**
  * Created by satuk on 10.07.17.
  */
+import React, {Component} from "react";
+import OrganizationList from "../../containers/OrganizationList";
+
+class Organizations extends Component {
+  render() {
+    return (
+      <OrganizationList />
+    );
+  }
+}
+
+export default Organizations;
