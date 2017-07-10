b6946e046418b2c7ac8595d44f95d921ff23780f
Satuk Kolan <34@satuk.ch>
2017-07-10 21:44:28 +0200

    fixed organization reducer

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,17 +1,16 @@
 /**
  * Created by satuk on 06.07.17.
  */
-import {ADD_EVENT, ADD_EVENTS} from "./constants";
+import {ADD_EVENT, ADD_EVENTS, ADD_ORGANIZATION, ADD_ORGANIZATIONS} from "./constants";
 import {combineReducers} from "redux";
 
 const events = (state = {}, action) => {
   switch ( action.type ) {
     case ADD_EVENTS:
-      const newState = action.payload.events.reduce((acc, event) => {
+      return  action.payload.events.reduce((acc, event) => {
         acc[event.id] = event;
         return acc;
       }, {});
-      return Object.assign({}, state, newState);
     case ADD_EVENT:
       const { event } = action.payload;
       return Object.assign({}, state, { [event.id]: event });
@@ -20,6 +19,22 @@ const events = (state = {}, action) => {
   }
 };
 
+const organizations = (state = {}, action) => {
+  switch ( action.type ) {
+    case ADD_ORGANIZATIONS:
+      return action.payload.organizations.reduce((acc, organization) => {
+        acc[organization.id] = organization;
+        return acc;
+      }, {});
+    case ADD_ORGANIZATION:
+      const { organization } = action.payload;
+      return Object.assign({}, state, { [organization.id]: organization });
+    default:
+      return state;
+  }
+};
+
 export default combineReducers({
   events,
+  organizations,
 });
