ac44580317a7e1105dd6906452a786b54a184a9c
Satuk Kolan <34@satuk.ch>
2017-07-10 21:40:29 +0200

    implemented the apply button

--- src/components/EventListItem/index.js
+++ src/components/EventListItem/index.js
@@ -4,17 +4,21 @@
 import React from "react";
 import {TableCell, TableRow} from "material-ui/Table";
 import moment from "moment";
-
+import Button from "material-ui/Button";
+import {Link} from "react-router-dom";
 
 const EventListItem = (props) => {
   const { event } = props;
   const eventDate = moment(event.eventDate).format('DD.MM.YYYY');
   const eventStart = moment(event.startEvent).format('HH:MM');
   const eventEnd = moment(event.eventEnd).format('HH:MM');
+  const org_id = event.company.id;
+  const event_id = event.id;
+
   return (
     <TableRow key={event.id}>
       <TableCell>
-        Org. name
+        {event.company.name}
       </TableCell>
       <TableCell>
         {event.eventName}
@@ -29,6 +33,13 @@ const EventListItem = (props) => {
         {
           `${eventStart} - ${eventEnd}`
         }
+      </TableCell>
+      <TableCell>
+        <Link to={'/organizations/' + org_id + '/events/' + event_id + '/apply'} style={{ textDecoration: 'none', }}>
+          <Button raised>
+            Apply
+          </Button>
+        </Link>
       </TableCell>
     </TableRow>
   );
