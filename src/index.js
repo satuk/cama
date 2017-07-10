742a0003f4435c94f6c273a5aa27406704dcba3c
Satuk Kolan <34@satuk.ch>
2017-07-10 21:42:01 +0200

    refactored and moved some components to the components folder; added new path

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 import {Provider} from "react-redux";
-import {MuiThemeProvider, createMuiTheme} from "material-ui/styles";
-import createPalette from 'material-ui/styles/palette';
+import {createMuiTheme, MuiThemeProvider} from "material-ui/styles";
+import createPalette from "material-ui/styles/palette";
 import injectTapEventPlugin from "react-tap-event-plugin";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 import Header from "./containers/Header";
 import Home from "./routes/Home";
-import EventList from "./containers/EventList";
+import EventList from "./routes/Events";
+import OrganizationList from "./routes/Organizations";
+import EventsByOrganization from "./routes/EventsByOrganization";
+import EventApplication from './routes/EventApplication';
 
 import store from "./store";
 
@@ -30,6 +33,11 @@ ReactDOM.render(
           <Header>
             <Route exact path={"/"} component={ Home }/>
             <Route exact path={"/events"} component={ EventList }/>
+            <Route exact path={"/organizations/:organization_id/events/:event_id/apply"} component={ EventApplication }/>
+            {/*<Route exact path={"/events/:id/events"} component={ EventsByOrganization }/>*/}
+            <Route exact path={"/organizations"} component={OrganizationList}/>
+            {/*<Route exact path={"/organizations/:id/apply"} component={}/>*/}
+            <Route exact path={"/organizations/:id/events"} component={ EventsByOrganization }/>
           </Header>
         </Switch>
       </Router>
