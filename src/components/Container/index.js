9de16264ca6cf87214f9121bf1044857d1831ae3
Satuk Kolan <34@satuk.ch>
2017-07-10 21:58:52 +0200

    removed react project and added static files for github pages

--- src/components/Container/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by satuk on 07.07.17.
- */
-import React from 'react';
-import './index.css';
-
-export default ({ children }) =>
-  <div className="Home-Container">{ children }</div>;
