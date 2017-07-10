858bd4b39b601bb2228e62119d26a3d126a94034
Satuk Kolan <34@satuk.ch>
2017-07-10 21:43:30 +0200

    created a organization creator

--- src/store/actions/organizations.js
+++ src/store/actions/organizations.js
@@ -1,3 +1,26 @@
 /**
  * Created by satuk on 10.07.17.
  */
+import {ADD_ORGANIZATION, ADD_ORGANIZATIONS, API} from "../constants";
+
+const addOrganization = (organization) => ({
+  type: ADD_ORGANIZATION,
+  payload: { organization },
+});
+
+const addOrganizations = (organizations) => ({
+  type: ADD_ORGANIZATIONS,
+  payload: { organizations },
+});
+
+export const fetchOrganizations = () => ({
+  type: API,
+  url: '/api/companies',
+  success: addOrganizations,
+});
+
+export const fetchOrganization = (id) => ({
+  type: API,
+  url: '/api/companies/' + id,
+  success: addOrganization,
+});
