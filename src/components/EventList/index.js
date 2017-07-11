740d2e6f8bfb73347a6898e30e9a9e254c10b092
Satuk Kolan <34@satuk.ch>
2017-07-11 19:03:10 +0200

    refactored and restyled the event list

--- src/components/EventList/index.js
+++ src/components/EventList/index.js
@@ -2,40 +2,66 @@
  * Created by satuk on 07.07.17.
  */
 import React from "react";
-import Table, {TableBody, TableCell, TableHead, TableRow} from "material-ui/Table";
-import Container from "../Container/index";
-import EventListItem from "../EventListItem/index";
-import Paper from "material-ui/Paper";
+import PropTypes from "prop-types";
+import EventListItem from "../../components/EventListItem";
+import Container from "../Container";
+import Typography from "material-ui/Typography";
+import {createStyleSheet, withStyles} from "material-ui/styles";
+import {Paper, TextField, Button} from "material-ui";
+import Icon from "material-ui/Icon";
+
+
+const styleSheet = createStyleSheet('EventList', theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  input: {
+    margin: theme.spacing.unit,
+  },
+  search: {
+    borderRadius: 20,
+  }
+}));
 
 const EventList = (props) => {
 
   const { events } = props;
+  const { classes } = props;
 
   return (
     <Container>
-      <Paper>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Organization</TableCell>
-              <TableCell>Event</TableCell>
-              <TableCell>Date</TableCell>
-              <TableCell>Functions</TableCell>
-              <TableCell>Time</TableCell>
-              <TableCell>Application</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {
-              events.map((event, index) => {
-                return <EventListItem key={index} event={event} size={events.length}/>
-              })
-            }
-          </TableBody>
-        </Table>
+      <Typography type="display2" style={{ color: '#424242', }}>All Events</Typography>
+      <Paper style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: '20px',
+        marginTop: '30',
+      }}>
+        <Icon style={{ padding: '20px 30px', color: 'white', fontSize: '52px', }}>search</Icon>
+        <TextField
+          id="search"
+          type="text"
+          InputProps={{ placeholder: 'Search upcomming Events' }}
+          fullWidth
+          marginForm
+          classes={classes.search}
+        />
       </Paper>
+      <div style={{marginTop: '30px',marginBottom: '45px',}}>
+        <Button raised color="primary" >All</Button>
+        <Button raised color="default">My Organizations</Button>
+      </div>
+      {
+        events.map((event, index) => <EventListItem key={index} event={event} size={events.length} classes={classes}/>)
+      }
     </Container>
   );
 };
 
-export default EventList;
+EventList.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styleSheet)(EventList);
--- src/components/EventListItem/index.js
+++ src/components/EventListItem/index.js
@@ -2,46 +2,50 @@
  * Created by satuk on 07.07.17.
  */
 import React from "react";
-import {TableCell, TableRow} from "material-ui/Table";
 import moment from "moment";
-import Button from "material-ui/Button";
+import {Button, Grid} from "material-ui";
 import {Link} from "react-router-dom";
+import Paper from "material-ui/Paper";
 
 const EventListItem = (props) => {
   const { event } = props;
-  const eventDate = moment(event.eventDate).format('DD.MM.YYYY');
-  const eventStart = moment(event.startEvent).format('HH:MM');
-  const eventEnd = moment(event.eventEnd).format('HH:MM');
+  const { classes } = props;
+
+  const date = moment(event.date).format('DD.MM.YYYY');
+  const start = moment(event.start).format('HH:MM');
+  const end = moment(event.end).format('HH:MM');
   const org_id = event.company.id;
   const event_id = event.id;
 
   return (
-    <TableRow key={event.id}>
-      <TableCell>
-        {event.company.name}
-      </TableCell>
-      <TableCell>
-        {event.eventName}
-      </TableCell>
-      <TableCell>
-        {eventDate}
-      </TableCell>
-      <TableCell>
-        {event.id}
-      </TableCell>
-      <TableCell>
-        {
-          `${eventStart} - ${eventEnd}`
-        }
-      </TableCell>
-      <TableCell>
-        <Link to={'/organizations/' + org_id + '/events/' + event_id + '/apply'} style={{ textDecoration: 'none', }}>
-          <Button raised>
-            Apply
-          </Button>
-        </Link>
-      </TableCell>
-    </TableRow>
+    <Paper style={{ marginTop: 24, }}>
+      <Grid container gutter={24} style={{color: 'white', display: 'flex', justifyContent: 'center', alignItems: 'center',}} >
+        <Grid item xs={12} md={3} >
+          <img src={event.logo}
+               style={{
+                 width: '100%',
+                 height: '125',
+                 objectFit: 'cover',
+                 overflow: 'hidden',
+                 paddingLeft: 12,
+               }}
+               alt={event.description}/>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <h1>{event.name}</h1>
+          <p>{event.description}</p>
+        </Grid>
+        <Grid item xs={12} md={3}>
+          <p>{event.company.name}</p>
+          <p>{event.company.address}</p>
+        </Grid>
+        <Grid item xs={12} md={2}>
+          <Link to={'/organizations/' + org_id + '/events/' + event_id + '/apply'} style={{ textDecoration: 'none', }}>
+            <Button raised>Apply</Button>
+          </Link>
+        </Grid>
+      </Grid>
+    </Paper>
   );
 };
 
