872a217dca67b4be46bfb001de639c984bb667b9
Satuk Kolan <34@satuk.ch>
2017-07-11 07:36:37 +0200

    extraced and restructured organizations component

--- src/routes/Organizations/index.js
+++ src/routes/Organizations/index.js
@@ -3,13 +3,45 @@
  */
 import React, {Component} from "react";
 import OrganizationList from "../../containers/OrganizationList";
+import {connect} from "react-redux";
+import {fetchOrganizations} from "../../store/actions/organizations";
+import {fetchEventsByCompany} from "../../store/actions/events";
+import Loading from "../../components/Loading";
+
 
 class Organizations extends Component {
+
+  componentDidMount() {
+    this.props.fetchOrganizations();
+  }
+
   render() {
+    const { classes } = this.props;
+    const { organizations } = this.props;
+
+    if ( !organizations.length ) {
+      return <Loading />;
+    }
+
     return (
-      <OrganizationList />
+      <OrganizationList classes={classes} organiizations={organizations}/>
     );
   }
 }
 
-export default Organizations;
+const mapSateToProps = (state) => {
+  const organizations = Object.values(state.organizations);
+  const eventsByCompany = Object.values(state.events);
+
+  return {
+    organizations,
+    eventsByCompany,
+  }
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchOrganizations: () => dispatch(fetchOrganizations()),
+  fetchEventsByCompany: (id) => dispatch(fetchEventsByCompany(id)),
+});
+
+export default connect(mapSateToProps, mapDispatchToProps)(Organizations);
