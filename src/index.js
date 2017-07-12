7e89bc92d342f5e362b985aca5e0f8d598fb7682
Satuk Kolan <34@satuk.ch>
2017-07-12 14:14:06 +0200

    defined global button, header and card style and override it

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import {colors} from './utils/theme';
 import registerServiceWorker from "./registerServiceWorker";
 import {Provider} from "react-redux";
 import {createMuiTheme, MuiThemeProvider} from "material-ui/styles";
@@ -13,7 +14,7 @@ import Home from "./routes/Home";
 import EventList from "./routes/Events";
 import OrganizationList from "./routes/Organizations";
 import EventsByOrganization from "./routes/EventsByOrganization";
-import EventApplication from './routes/EventApplication';
+import EventApplication from "./routes/EventApplication";
 
 import store from "./store";
 
@@ -21,6 +22,51 @@ const theme = createMuiTheme({
   palette: createPalette({
     type: 'dark',
   }),
+  overrides: {
+    Header: {
+      root: {
+        backgroundColor: colors.dark3,
+      },
+    },
+    MuiToolbar: {
+      root: {
+        backgroundColor: colors.dark1,
+      },
+    },
+    MuiButton: {
+      raised: {
+        background: 'linear-gradient(45deg, ' + colors.orange + ' 30%, ' + colors.yellow + ' 90%)',
+        borderRadius: 3,
+        border: 0,
+        color: colors.dark1 + '!important',
+        height: 40,
+        padding: '0 30px',
+        boxShadow: '0 3px 5px 2px' + colors.dark3,
+      },
+      raisedPrimary: {
+        background: 'linear-gradient(45deg, ' + colors.dark2 + ' 30%, ' + colors.dark3 + ' 90%)',
+        borderRadius: 3,
+        border: 0,
+        color: colors.yellow + '!important',
+        height: 40,
+        padding: '0 30px',
+        boxShadow: '0 3px 5px 2px' + colors.dark3,
+      },
+    },
+    MuiCardActions: {
+      root: {
+        backgroundColor: colors.dark4,
+        justifyContent: 'space-around',
+        padding: '5px 0',
+      },
+    },
+    MuiCardContent: {
+      root: {
+        height: '125px',
+        paddingBottom: 20,
+      },
+    },
+  },
 });
 
 injectTapEventPlugin();
@@ -33,7 +79,8 @@ ReactDOM.render(
           <Header>
             <Route exact path={"/"} component={ Home }/>
             <Route exact path={"/events"} component={ EventList }/>
-            <Route exact path={"/organizations/:organization_id/events/:event_id/apply"} component={ EventApplication }/>
+            <Route exact path={"/organizations/:organization_id/events/:event_id/apply"}
+                   component={ EventApplication }/>
             {/*<Route exact path={"/events/:id/events"} component={ EventsByOrganization }/>*/}
             <Route exact path={"/organizations"} component={OrganizationList}/>
             {/*<Route exact path={"/organizations/:id/apply"} component={}/>*/}
