3b0880d3ed7c77bf76aeb0e72cd65536c14555d7
Satuk Kolan <34@satuk.ch>
2017-07-12 18:37:10 +0200

    added new constants

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -8,5 +8,15 @@ export const colors = {
   dark4: '#575757',
   yellow: '#FFB84E',
   orange: '#FF9902',
+  light: '#C0C0C0',
 };
+
+export const gradients = {
+  orangeToYellow: 'linear-gradient(45deg, ' + colors.orange + ' 30%, ' + colors.yellow + ' 90%)',
+  dark1ToDark4: 'linear-gradient(45deg, ' + colors.dark1 + ' 30%, ' + colors.dark4 + ' 90%)',
+  dark2ToDark3: 'linear-gradient(45deg, ' + colors.dark2 + ' 30%, ' + colors.dark3 + ' 90%)',
+  dark3ToDark2: 'linear-gradient(45deg, ' + colors.dark3 + ' 30%, ' + colors.dark2 + ' 90%)',
+  dark4ToDark1: 'linear-gradient(45deg, ' + colors.dark4 + ' 30%, ' + colors.dark1 + ' 90%)',
+};
+
 
