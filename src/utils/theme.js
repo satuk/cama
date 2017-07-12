899d09b71d7468091b81d81de1c12aa0d6e9ffa9
Satuk Kolan <34@satuk.ch>
2017-07-12 14:12:07 +0200

    created theme color constants

---
+++ src/utils/theme.js
@@ -0,0 +1,12 @@
+/**
+ * Created by satuk on 12.07.17.
+ */
+export const colors = {
+  dark1: '#222222',
+  dark2: '#252525',
+  dark3: '#353739',
+  dark4: '#575757',
+  yellow: '#FFB84E',
+  orange: '#FF9902',
+};
+
