d9a6f06a303c48d91d3c0e9b5f6027d51ca834a0
Satuk Kolan <34@satuk.ch>
2017-07-13 15:06:12 +0200

    refactored event by organization page; added a text message if a company doesnt have events;

--- src/routes/EventsByOrganization/index.js
+++ src/routes/EventsByOrganization/index.js
@@ -5,25 +5,79 @@ import React, {Component} from "react";
 import EventList from "../../components/EventList/index";
 import {connect} from "react-redux";
 import {fetchEvents} from "../../store/actions/events";
+import {fetchOrganization} from "../../store/actions/organizations";
 import Loading from "../../components/Loading";
+import Container from "../../components/Container";
+import Logo from "../../components/OrganizationLogo";
+import {Grid, Paper, Typography} from "material-ui";
+import {gradients} from "../../utils/theme";
+
+const styles = {
+  paper: {
+    padding: 50,
+  },
+};
 
 class EventsByOrganization extends Component {
-
   componentDidMount() {
     if ( !this.props.eventsByCompany.length ) {
       this.props.fetchEvents();
+      this.props.fetchOrganization(this.props.match.params.id);
     }
   }
 
   render() {
-    const { eventsByCompany } = this.props;
 
-    if ( !this.props.eventsByCompany ) {
+    if ( !Object.keys(this.props.organization).length ) {
       return <Loading />;
     }
 
+    const { eventsByCompany } = this.props;
+    const { id } = this.props.match.params;
+    const organization = this.props.organization[id];
+
     return (
-      <EventList events={eventsByCompany}/>
+      <Container>
+        <Grid container gutter={24}>
+          <Grid item lg={3}>
+            <Logo src={organization.logo} alt={organization.name} style={{
+              background: gradients.dark4ToDark1, margin: '20px' +
+              ' 20px 20px 0',
+            }}/>
+          </Grid>
+          <Grid item lg>
+            <Typography type="display1" gutterBottom>
+              {organization.name}
+            </Typography>
+            <Typography type="caption" gutterBottom>
+              {organization.description}
+            </Typography>
+            <Typography type="title" gutterBottom>
+              {`${organization.address}, ${organization.postalCode} ${organization.city}`}
+            </Typography>
+            <Typography type="title" gutterBottom>
+              <a href={organization.url} target="_blank">{organization.url}</a>
+            </Typography>
+          </Grid>
+        </Grid>
+        {eventsByCompany.length ? (
+          <div style={{margin: '50px 0',}}>
+            <Typography type="display1" gutterBottom>
+              Up coming Events
+            </Typography>
+            <EventList events={eventsByCompany}/>
+          </div>
+        ) : (
+          <Container>
+            <Paper style={styles.paper}>
+              <Typography type="title">
+                {`The Company ${organization.name} has no events...`}
+              </Typography>
+            </Paper>
+          </Container>
+        )}
+
+      </Container>
     );
   }
 }
@@ -31,13 +85,16 @@ class EventsByOrganization extends Component {
 const mapStateToProps = (state, props) => {
   const eventsByCompany = Object.values(state.events)
     .filter(event => event.company.id == props.match.params.id);
+  const organization = state.organizations;
 
   return {
     eventsByCompany,
+    organization,
   }
 };
 const mapDispatchToProps = (dispatch) => ({
   fetchEvents: () => dispatch(fetchEvents()),
+  fetchOrganization: (id) => dispatch(fetchOrganization(id)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(EventsByOrganization);
