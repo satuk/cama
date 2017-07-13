81a6795e5496190728a1881146ccbd9174211c02
Satuk Kolan <34@satuk.ch>
2017-07-13 13:35:03 +0200

    seperated the searchbox for events

--- src/components/EventList/index.js
+++ src/components/EventList/index.js
@@ -4,12 +4,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import EventListItem from "../../components/EventListItem";
-import Container from "../Container";
-import Typography from "material-ui/Typography";
 import {createStyleSheet, withStyles} from "material-ui/styles";
-import {Button, Paper, TextField} from "material-ui";
-import Icon from "material-ui/Icon";
-
 
 const styleSheet = createStyleSheet('EventList', theme => ({
   container: {
@@ -19,9 +14,6 @@ const styleSheet = createStyleSheet('EventList', theme => ({
   input: {
     margin: theme.spacing.unit,
   },
-  search: {
-    borderRadius: 20,
-  }
 }));
 
 const EventList = (props) => {
@@ -29,33 +21,11 @@ const EventList = (props) => {
   const { classes } = props;
 
   return (
-    <Container>
-      <Typography type="display2">All Events</Typography>
-      <Paper style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: '20px',
-        marginTop: '30',
-      }}>
-        <Icon style={{ padding: '20px 30px', color: 'white', fontSize: '52px', }}>search</Icon>
-        <TextField
-          id="search"
-          type="text"
-          InputProps={{ placeholder: 'Search upcoming Events' }}
-          fullWidth
-          marginForm
-          classes={classes.search}
-        />
-      </Paper>
-      <div style={{ marginTop: '30px', marginBottom: '45px', }}>
-        <Button raised color="primary">All</Button>
-        <Button raised color="primary">My Organizations</Button>
-      </div>
+    <div>
       {
-        events.map((event, index) => <EventListItem key={index} event={event} size={events.length} classes={classes}/>)
+        events.map(e => <EventListItem key={e.id} event={e} size={e.length} classes={classes}/>)
       }
-    </Container>
+    </div>
   );
 };
 
--- src/routes/Events/index.js
+++ src/routes/Events/index.js
@@ -6,6 +6,16 @@ import EventList from "../../components/EventList";
 import {fetchEvents} from "../../store/actions/events";
 import {connect} from "react-redux";
 import Loading from "../../components/Loading";
+import Container from "../../components/Container";
+import Typography from "material-ui/Typography";
+import {Button, Paper, TextField} from "material-ui";
+import Icon from "material-ui/Icon";
+
+const styles = {
+  search: {
+    borderRadius: 20,
+  },
+};
 
 class Events extends Component {
   componentDidMount() {
@@ -20,7 +30,31 @@ class Events extends Component {
     }
 
     return (
-      <EventList events={events}/>
+      <Container>
+        <Typography type="display2">All Events</Typography>
+        <Paper style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderRadius: '20px',
+          marginTop: '30',
+        }}>
+          <Icon style={{ padding: '20px 30px', color: 'white', fontSize: '52px', }}>search</Icon>
+          <TextField
+            id="search"
+            type="text"
+            InputProps={{ placeholder: 'Search upcoming Events' }}
+            fullWidth
+            marginForm
+            style={styles.search}
+          />
+        </Paper>
+        <div style={{ marginTop: '30px', marginBottom: '45px', }}>
+          <Button raised color="primary">All</Button>
+          <Button raised color="primary">My Organizations</Button>
+        </div>
+        <EventList events={events}/>
+      </Container>
     );
   }
 }
